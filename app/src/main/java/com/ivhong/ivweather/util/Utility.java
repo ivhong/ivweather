94c6a5981ad1b2d149208be37bf9703acf76c85f
ivhong <hong350@163.com>
2018-01-02 14:57:30 +0800

    完成便利省市县三级列表的功能

---
+++ app/src/main/java/com/ivhong/ivweather/util/Utility.java
@@ -0,0 +1,66 @@
+package com.ivhong.ivweather.util;
+
+import android.text.TextUtils;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+import com.ivhong.ivweather.db.City;
+import com.ivhong.ivweather.db.County;
+import com.ivhong.ivweather.db.Province;
+
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by wangchanghong on 2017/12/29.
+ */
+
+public class Utility {
+    public static boolean handleProvinceResponse(String response){
+        if(!TextUtils.isEmpty(response)){
+            ArrayList<JsonObject> jsonObjects = new Gson().fromJson(response, new TypeToken<ArrayList<JsonObject>>(){}.getType());
+            for(JsonObject jsonObject:jsonObjects){
+                Province province = new Province();
+                province.setProvinceName(jsonObject.get("name").getAsString());
+                province.setProvinceCode(jsonObject.get("id").getAsInt());
+                province.save();
+            }
+        }
+        return true;
+    }
+
+
+    public static boolean handleCityResponse(String response, int provinceId){
+        if(!TextUtils.isEmpty(response)){
+            ArrayList<JsonObject> jsonObjects = new Gson().fromJson(response, new TypeToken<ArrayList<JsonObject>>(){}.getType());
+            for(JsonObject jsonObject:jsonObjects){
+                City city = new City();
+                city.setCityName(jsonObject.get("name").getAsString());
+                city.setCityCode(jsonObject.get("id").getAsInt());
+                city.setProvinceId(provinceId);
+                city.save();
+            }
+        }
+        return true;
+    }
+
+
+    public static boolean handleCountyResponse(String response, int cityId){
+        if(!TextUtils.isEmpty(response)){
+            ArrayList<JsonObject> jsonObjects = new Gson().fromJson(response, new TypeToken<ArrayList<JsonObject>>(){}.getType());
+            for(JsonObject jsonObject:jsonObjects){
+                County county = new County();
+                county.setCountyName(jsonObject.get("name").getAsString());
+                county.setWeatherId(jsonObject.get("weather_id").getAsString());
+                county.setCityId(cityId);
+                county.save();
+            }
+        }
+        return true;
+    }
+}
